b2c1519bb37dc15a3301f1c4645c34ffe902b26d
Gabrielopes02 <tecnicogabrielopes@gmail.com>
2026-08-20 17:18:44 -0300

    alteracoes em casa

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const addTable = document.querySelector('#adicionar')
+const addTable = document.querySelector("#adicionar");
+const saveInPdf =document.querySelector('#saveInPdf')
+addTable.addEventListener("click", () => {
+  const tableQnt = document.querySelector("#tableQnt");
+  const tableDesc = document.querySelector("#tableDesc");
+  const tableValUni = document.querySelector("#tableValUni");
+  const tableTot = document.querySelector("#tableTot");
+  const ul = document.querySelector("#ulList");
 
- 
-addTable.addEventListener('click',()=>{
-  const tableQnt = document.querySelector('#tableQnt')
-const tableDesc = document.querySelector('#tableDesc')
-const tableValUni = document.querySelector('#tableValUni')
-const tableTot = document.querySelector('#tableTot')
-  const ul = document.querySelector('#ulList')
+  const newLi = document.createElement("li");
   
-  const newLi = document.createElement('li')
+  newLi.innerHTML = `
+          <div class="flex flex-wrap" >
+          <div class="w-1/10 flex justify-center items-center">${tableQnt.value}</div>
+          <div class="w-7/10 text-justify p-5 flex justify-center items center "><p>${tableDesc.value}</p></div>
+          <div class="w-1/10 flex justify-center items-center">${tableValUni.value}</div>
+          <div class="w-1/10 flex justify-center items-center">${tableTot.value}</div>
+          </div>`;
+
   
-  newLi.innerHTML = `<div class="flex justify-between mx-2">
-          
-        <div class="w-1/10 bg-green-100">${tableQnt.value}</div>
-          <div class="break-words">${tableDesc.value}</div>
-          <div class="w-1/10 bg-green-100">${tableValUni.value}</div>
-          <div class="w-1/10">${tableTot.value}</div>
-        </div>`
+  newLi.classList.add('border')
+  ul.append(newLi);
+
+  tableQnt.value = "";
+  tableDesc.value = "";
+  tableValUni.value = "";
+  tableTot.value = "";
+
+  console.log(tableQnt, tableDesc, tableValUni, tableTot);
+});
+saveInPdf.addEventListener('click', () => {
   
-  ul.append(newLi)
-  
-  tableQnt.value=""
-  tableDesc.value=''
-  tableValUni.value=''
-  tableTot.value=''
-  
-  console.log(tableQnt,tableDesc,tableValUni,tableTot)
+  window.print()
 })
