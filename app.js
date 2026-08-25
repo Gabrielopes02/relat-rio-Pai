f8fa08a8f632202cc4e3a94a69b1960efcf14aad
Gabrielopes02 <tecnicogabrielopes@gmail.com>
2026-08-25 03:52:46 -0300

    adicionando break all depois de ter infartado

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ addTable.addEventListener("click", () => {
 
   newDiv.innerHTML = `<div class="flex p-1 w-full gap-2">
           <div class=" w-full max-w-1/10 flex break-all">${tableQnt.value}</div>
-          <div class="w-full flex justify-center wrap text-justify">${tableDesc.value}</div>
+          <div class="w-full flex justify-center wrap text-justify break-all">${tableDesc.value}</div>
           <div class="w-2/10 text-center">${tableValUni.value}</div>
           <div class="w-2/10 flex justify-center">${tableTot.value}</div>
         </div>
