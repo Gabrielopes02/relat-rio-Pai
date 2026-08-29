373927c96848c9a17096c4a4d184fc1b33afc35f
Gabrielopes02 <tecnicogabrielopes@gmail.com>
2026-08-29 12:22:25 -0300

    colocando alterações até delete/edit da table

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 /*
 to do list
-colocar tabela pra fazer as contas
+
+Map separado [check]
+Bobina minúscula [check]
+Inverter input com total
+Editar tabela
+mudar nome do pdf
 
 */
 
 const addTable = document.querySelector("#adicionar");
 const saveInPdf = document.querySelector("#saveInPdf");
 let arrayTotal = [];
+const inputPressionB = document.querySelector("#baixaBar");
+const inputPressionBPa = document.querySelector("#baixaMPa");
+const inputPressionA = document.querySelector("#altaBar");
+const inputPressionAKPa = document.querySelector("#altaKPa");
+
+const sensorMapBar = document.querySelector("#sensorMapBar");
+const sensorMapPa = document.querySelector("#sensorMapPa");
+const sensorMafBar = document.querySelector("#sensorMafBar");
+const sensorMafPa = document.querySelector("#sensorMafPa");
+const inputs = [
+  inputPressionA,
+  inputPressionAKPa,
+  inputPressionB,
+  inputPressionBPa,
+  sensorMapBar,
+  sensorMapPa,
+  sensorMafBar,
+  sensorMafPa,
+];
+
+let unidadesMedida = [
+  [1, 100],
+  [0.01, 1],
+  [1, 10],
+  [0.1, 1],
+];
 
 addTable.addEventListener("click", () => {
   const tableQnt = document.querySelector("#tableQnt");
@@ -18,23 +49,33 @@ addTable.addEventListener("click", () => {
 
   const newDiv = document.createElement("div");
   let total = 0;
-
+  const table = document.querySelector("#table");
   divTotalExist = document.querySelector("#divTotal");
   newDiv.innerHTML = `<div class="flex p-1 w-full gap-2">
-          <div class=" w-full max-w-1/10 flex break-all">${tableQnt.value}</div>
+          <div class=" w-full max-w-1/10 flex break-all nowrap">${tableQnt.value}</div>
           <div class="w-full flex justify-center wrap text-justify break-all">${tableDesc.value}</div>
           <div class="w-2/10 text-center">R$ ${tableValUni.value == "" ? "00,00" : tableValUni.value}</div>
           <div class="w-2/10 flex justify-center">R$ ${tableQnt.value * tableValUni.value == 0 ? "00,00" : tableQnt.value * tableValUni.value}</div>
-        </div>`;
+         <div class="w-1/10">
+            <i
+              class="fa-regular fa-pen-to-square text-white hover:bg-gray-400 rounded text-lg ml-1"
+              onclick="editTable(event)"></i>
+            <i
+              class="fa-regular fa-trash-can text-lg text-red-400 hover:bg-gray-400 rounded"
+              onclick="deleteTable(event)"></i>
+          </div>
+          </div>
+         `;
 
   newDiv.classList.add("border");
   divList.append(newDiv);
 
   if (divTotalExist) {
-    divList.appendChild(divTotalExist);
+    table.appendChild(divTotalExist);
   } else {
     let divTotal = document.createElement("div");
     divTotal.innerHTML = `<div class="flex w-full mt-1">
+    
           <div class=" w-full max-w-1/10 flex break-all"></div>
           <div class="w-full flex justify-center wrap text-justify break-all"></div>
           <div class="w-2/10 text-center border border-r-0">Total</div>
@@ -42,11 +83,10 @@ addTable.addEventListener("click", () => {
         </div>`;
 
     divTotal.setAttribute("id", "divTotal");
-    divList.append(divTotal);
+    table.append(divTotal);
   }
   arrayTotal.push(tableQnt.value * tableValUni.value);
 
-  console.log(divTotal.children[0].children[3]);
   tableQnt.value = "";
   tableDesc.value = "";
   tableValUni.value = "";
@@ -59,18 +99,6 @@ addTable.addEventListener("click", () => {
 saveInPdf.addEventListener("click", () => {
   window.print();
 });
-
-const inputPressionB = document.querySelector("#baixaBar");
-const inputPressionBPa = document.querySelector("#baixaMPa");
-const inputPressionA = document.querySelector("#altaBar");
-const inputPressionAKPa = document.querySelector("#altaKPa");
-
-let unidadesMedida = [
-  [1, 100],
-  [0.01, 1],
-  [1, 10],
-  [0.1, 1],
-];
 
 inputPressionB.addEventListener("change", () => {
   let value = Number(inputPressionB.value);
@@ -107,16 +135,7 @@ inputPressionAKPa.addEventListener("change", () => {
   inputPressionA.value = `${convertTomBar} Bar`;
   inputPressionAKPa.value = `${convertToKPa} Kpa`;
 });
-const sensorMapBar = document.querySelector("#sensorMapBar");
-const sensorMapPa = document.querySelector("#sensorMapPa");
-const inputs = [
-  inputPressionA,
-  inputPressionAKPa,
-  inputPressionB,
-  inputPressionBPa,
-  sensorMapBar,
-  sensorMapPa,
-];
+
 inputs.forEach((input) => {
   input.addEventListener("click", () => (input.value = ""));
 });
@@ -136,3 +155,44 @@ sensorMapPa.addEventListener("change", () => {
   sensorMapBar.value = `${valuemBar} mBar`;
   sensorMapPa.value = `${valueKPa} KPa`;
 });
+
+sensorMafBar.addEventListener("change", () => {
+  let value = sensorMafBar.value;
+  let valuemBar = value * unidadesMedida[2][0];
+  let valueKPa = value * unidadesMedida[2][1];
+  sensorMafBar.value = `${valuemBar} mBar`;
+  sensorMafPa.value = `${valueKPa} KPa`;
+});
+sensorMafPa.addEventListener("change", () => {
+  let value = sensorMafPa.value;
+  let valuemBar = value * unidadesMedida[3][0];
+  let valueKPa = value * unidadesMedida[3][1];
+  sensorMafBar.value = `${valuemBar} mBar`;
+  sensorMafPa.value = `${valueKPa} KPa`;
+});
+
+const editTable = (event) => {
+  let divEdit = Array.from(
+    event.currentTarget.parentElement.parentElement.children,
+  );
+  divEdit.forEach((d, i) => {
+    if (i == divEdit.length - 1) {
+      d.innerHTML = `<i class="fa-solid fa-check text-green-400 hover:bg-gray-500 rounded text-lg"></i><i
+              class="fa-regular fa-trash-can text-lg text-red-400 hover:bg-gray-400 rounded"
+              onclick="deleteTable(event)"></i>`;
+      d.setAttribute('class',' flex gap-2 items-center')
+      console.log(d);
+    } else {
+      let content = d.textContent;
+      d.innerHTML = ``;
+      let newDiv = document.createElement("div");
+      newDiv.innerHTML = `<input type="text" value ="${content}" class= "w-full bg-gray-500 text-center rounded"/>`;
+      newDiv.setAttribute("class", "flex rounded-xl animate-pulse");
+      d.appendChild(newDiv);
+    }
+  });
+};
+const deleteTable = () => {
+  let divEdit = event.currentTarget.parentElement.parentElement.parentElement;
+  divEdit.remove();
+};
